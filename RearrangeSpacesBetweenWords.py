4994e41affe469a9ce024616cfbd10077e381a84
Bhumika Peshwani <bhumika15peshwani8@gmail.com>
2024-10-25 11:09:43 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ RearrangeSpacesBetweenWords.py
@@ -0,0 +1,27 @@
+"""You are given a string text of words that are placed among some number of spaces. Each word consists of one or more lowercase English letters and are separated by at least one space. It's guaranteed that text contains at least one word.
+
+Rearrange the spaces so that there is an equal number of spaces between every pair of adjacent words and that number is maximized. If you cannot redistribute all the spaces equally, place the extra spaces at the end, meaning the returned string should be the same length as text.
+
+Return the string after rearranging the spaces."""
+from itertools import count
+
+
+class Solution(object):
+    def reorderSpaces(self, text):
+
+        ## O(n) time and space complexity
+        space_count= text.count(" ")
+        words= text.split()
+        word_count= len(words)
+
+        if word_count==1:
+            return words[0] + " " * space_count
+
+        even_space= " "* (space_count// (word_count-1))
+        extra_space= " "* (space_count %(word_count-1))
+
+        return even_space.join(words)+ extra_space
+
+
+sol=Solution()
+print(sol.reorderSpaces(" practice   makes   perfect"))
