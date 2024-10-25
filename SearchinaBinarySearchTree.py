9f189c71e9cb67217dfe8077ced36de816c9dad6
Bhumika Peshwani <bhumika15peshwani8@gmail.com>
2024-10-26 00:43:07 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ SearchinaBinarySearchTree.py
@@ -0,0 +1,27 @@
+"""You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: Optional[TreeNode]
+        :type val: int
+        :rtype: Optional[TreeNode]
+        """
+        ## O(h) time complexity where h is height of tree and O(1) space complexity
+        while root is not None:
+            if root.val == val:
+                return root
+            elif val < root.val:
+                root = root.left
+            else:
+                root = root.right
+        return None
