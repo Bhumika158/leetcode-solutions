e3e643e2a9411cd748e536f2960bb8fe434c5835
Bhumika Peshwani <bhumika15peshwani8@gmail.com>
2024-11-06 17:56:58 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ BalancedBinaryTree.py
@@ -0,0 +1,28 @@
+"""Given a binary tree, determine if it is
+height-balanced
+."""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+
+        def helper(root):
+            if not root:
+                return [True, 0]
+
+            left, right = helper(root.left), helper(root.right)
+            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return helper(root)[0]
